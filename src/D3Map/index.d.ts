b5eddddc381d7b67b5c362e44b0db532452bae9e
Olivier St-Louis-Desjardins <Olivier.St-Louis-Desjardins@radio-canada.ca>
2019-06-19 15:36:24 -0400

    Fix typescript and publish 1.0.3

--- src/D3Map/index.d.ts
+++ src/D3Map/index.d.ts
@@ -1,25 +1,4 @@
-import React from 'react';
-
-export type D3MapProps = {
-    allowInteractivity?: boolean, // this is currently splitted
-    allParties?: any[],
-    allRidings?: any[],
-    currentRidingId?: number,
-    electionId: string,
-    e6nHardcodedRidingIdFix?: number,
-    E6N_PAGE_IDS?: any,
-    E6NToolTip?: any,
-    forwardMapRef?: any,
-    isWidget?: boolean,
-    isRidingOpen?: boolean,
-    mapData: any,
-    mapDOMContextId: string,
-    mapId: string,
-    setCurrentRiding?: VoidFunction,
-    setCurrentRidingTooltip?: VoidFunction,
-    styling?: any,
-    ZoomOutButton?: any,
-}
+export type D3MapProps = any;
 
 declare const D3Map: React.ComponentType<D3MapProps>;
 
